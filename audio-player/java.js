20e5af0721961219593f25a59616766c5bf943bf
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-09-19 12:05:42 +0300

    feat: add functional for change img, songs, singers and flipping(Tue, Sep 19, 2023 12:05:42 PM)

--- audio-player/java.js
+++ audio-player/java.js
@@ -1,19 +1,31 @@
 const audio = document.querySelector("audio");
 const playBtn = document.querySelector(".play");
 const pauseBtn = document.querySelector(".pause");
+const BtnBack = document.querySelector(".back");
+const BtnNext = document.querySelector(".next");
 const progress = document.querySelector("#progress");
+const backAll = document.querySelector(".background");
+const backCover = document.querySelector(".cover");
+const singer = document.querySelector(".singer");
+const titleSong = document.querySelector(".title_song");
 let isPlay = false;
 function playAudio() {
   if (!isPlay) {
+    play();
     audio.currentTime = 0;
-    audio.play();
     isPlay = true;
-    playBtn.src = "svg/pause.png";
   } else {
-    audio.pause();
     isPlay = false;
-    playBtn.src = "svg/play.png";
+    pause();
   }
+}
+function play() {
+  audio.play();
+  playBtn.src = "svg/pause.png";
+}
+function pause() {
+  playBtn.src = "svg/play.png";
+  audio.pause();
 }
 playBtn.addEventListener("click", playAudio);
 //change line
@@ -24,11 +36,33 @@ progress.addEventListener("input", function () {
 //buttons next and back
 let playNum = 0;
 const songs = ["I wanna love you", "understand", "Malang"];
+const singers = ["Akon", "BoyWithUke", "Aamir Khan"];
+function PlaySong(song) {
+  audio.src = `muz/${song}.mp3`;
+  backAll.src = `img/img${playNum + 1}.jpg`;
+  backCover.src = `img/img${playNum + 1}.jpg`;
+  singer.textContent = singers[playNum];
+  titleSong.textContent = song;
+}
 function playNext() {
   playNum++;
-  playAudio();
+  if (playNum > songs.length - 1) {
+    playNum = 0;
+  }
+
+  PlaySong(songs[playNum]);
+  play();
+  isPlay = true;
 }
+BtnNext.addEventListener("click", playNext);
 function playPrev() {
   playNum--;
-  playAudio();
+  if (playNum < 0) {
+    playNum = songs.length - 1;
+  }
+
+  PlaySong(songs[playNum]);
+  play();
+  isPlay = true;
 }
+BtnBack.addEventListener("click", playPrev);
